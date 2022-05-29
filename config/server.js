3aa5f1e16f7ad376af32f51a98c707d9f4f76d6c
Kużdowicz Piotr <piotr.kuzdowicz@streamsoft.pl>
2022-05-29 21:04:05 +0200

    api keys

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',['GFh/TyrZuzIHH/p2DY7NNg==','dFdwU0f+t71Tu4IxAeAq7A==']),
   },
 });
